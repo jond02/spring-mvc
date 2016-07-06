c6b28c3c5c8ec88ae262f9f353b51ab55b3f440a
Jonathan Dann <nwjon122@gmail.coml>
2016-07-05 23:26:17 -0700

    model basics

---
+++ src/main/java/com/nwjon/mvc/controllers/HomeModel.java
@@ -0,0 +1,28 @@
+package com.nwjon.mvc.controllers;
+
+import data.entities.Project;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jondann on 7/5/16.
+ */
+
+@Controller
+public class HomeModel {
+
+    @RequestMapping("/home-model")
+    public String goHomeModel(Model model){
+
+        Project project = new Project();
+        project.setName("First Time Project");
+        project.setSponsor("Home Builders");
+        project.setDescription("We like to build houses!");
+
+        model.addAttribute("currentProject", project);
+
+        return "home-model";
+    }
+}
+
